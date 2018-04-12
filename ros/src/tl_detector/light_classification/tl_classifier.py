e82b7689eac665ef00bdb093e92198fa47357e5d
Felipe Reis <falreis@gmail.com>
2018-04-12 20:36:07 -0300

    Create simulation for traffic lights

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,14 @@
 from styx_msgs.msg import TrafficLight
+import random
+import rospy
+import time
 
 class TLClassifier(object):
     def __init__(self):
+        self.current_light = TrafficLight.UNKNOWN
+        self.last_time = rospy.get_time()
+        rospy.logerr('Traffic Light UNKNOWN')
         #TODO load classifier
-        pass
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +21,23 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        '''
+        Traffic light simulator just to test the behavior of the vehicle
+        '''
+        if self.current_light == TrafficLight.RED:
+            if (rospy.get_time() - self.last_time) > 10:
+                self.current_light = TrafficLight.GREEN
+                rospy.logerr('Traffic Light GREEN')
+        else:
+            self.last_time = rospy.get_time()
+            randvalue = random.randint(0,1000)
+            
+            if randvalue < 2:
+                self.current_light = TrafficLight.RED
+                rospy.logerr('Traffic Light RED')
+            elif randvalue > 999:
+                self.current_light = TrafficLight.UNKNOWN
+            else:
+                self.current_light = TrafficLight.GREEN
+
+        return self.current_light
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,77 @@
+from pid import PID
+from lowpass import LowPassFilter
+from yaw_controller import YawController
+import math
+import rospy
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-
+MIN_SPEED =  0.1
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, 
+                wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
+        
         # TODO: Implement
-        pass
+        self.yaw_controller = YawController(wheel_base, steer_ratio, MIN_SPEED, max_lat_accel, max_steer_angle)
 
-    def control(self, *args, **kwargs):
+        kp = 0.3
+        ki = 0.1
+        kd = 0.
+        mn = 0.     # minimum throttle value
+        mx = 0.2    # maximum throttle value
+        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+
+        tau = 0.5   # 1/(2*pi*tau) = cutoff frequency
+        ts = 0.02   #sample time
+        self.vel_lpf = LowPassFilter(tau, ts)
+
+        self.vehicle_mass = vehicle_mass
+        self.fuel_capacity = fuel_capacity
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
+
+        self.last_time = rospy.get_time()
+    #end __init__
+        
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            throttle=brake=steering=0
+        else:
+            current_vel = self.vel_lpf.filt(current_vel)
+            
+            #rospy.logwarn("Angular vel: {0}", format(angular_vel))
+            #rospy.logwarn("Target velocity: {0}", format(linear_vel))
+            #rospy.logwarn("Target angular velocity: {0}\n", format(angular_vel))
+            #rospy.logwarn("Current velocity: {0}", format(current_vel))
+            #rospy.logwarn("Filtered velocity: {0}", format(self.vel._lpf.get()))
+
+            steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+
+            vel_error = linear_vel - current_vel
+            self.last_vel = current_vel
+
+            throttle = self.throttle_controller.step(vel_error, sample_time)
+            brake = 0
+
+            if linear_vel == 0. and current_vel <= MIN_SPEED:
+                throttle = 0
+                brake = self.vehicle_mass * self.wheel_radius #N*m - to hold de car in place if we are stopped at light - Acceleration: 1m/s2
+            elif throttle <= MIN_SPEED and vel_error < 0:
+                throttle = 0
+                decel = max(vel_error, self.decel_limit)
+                brake = abs(decel) * self.vehicle_mass * self.wheel_radius #Torque n*m
+
+        return throttle, brake, steering
+    #end control
+
